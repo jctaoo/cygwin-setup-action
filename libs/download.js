1d66e25ba4f2e7af04d8e2431864e746b6c4de9c
jctaoo <jctaoo@outlook.com>
2023-01-20 08:59:55 +0800

    fix bug

--- libs/download.js
+++ libs/download.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 module.exports.downloadFile = async (url, output) => {
     const res = await fetch(url);
-    const fileStream = fs.createWriteStream(path);
+    const fileStream = fs.createWriteStream(output);
     await new Promise((resolve, reject) => {
         res.body.pipe(fileStream);
         res.body.on("error", reject);
